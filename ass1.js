c2fa5755119349e68328e1fdc6402010c8fd6a44
karansaini47 <karan0saini23@gmail.com>
2025-12-06 12:51:22 +0530

    currenttimeprint

--- ass1.js
+++ ass1.js
@@ -1,16 +1,5 @@
-class animal {
-    constructor(name, legcount, speaks) {
-        this.name = name
-        this.legcount = legcount
-        this.speaks = speaks
-
-    }
-speak(){
-     console.log("hi there" + this.speaks)
+function currenttimeprint(){
+    console.log(new Date().getTime());
 }
 
-}
-let dog = new animal("dog",4, "bhow bhow");
-let cat = new animal("cat",4,"meow meow");
-
-cat.speak();
+setInterval(currenttimeprint, 1000);
