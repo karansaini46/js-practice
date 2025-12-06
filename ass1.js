1c90b9c9e62b5220e755a213999001a7518badf7
karansaini47 <karan0saini23@gmail.com>
2025-12-06 12:34:48 +0530

    class & methods

--- ass1.js
+++ ass1.js
@@ -1,30 +1,16 @@
-/*
-Write a function that calculates the time (in seconds) it takes for the JS code to calculate sum from 1 to n, given n as the input.
-Try running it for
-1. Sum from 1-100
-2. Sum from 1-100000
-3. Sum from 1-1000000000
-Hint - use Date class exposed in JS
-There is no automated test for this one, this is more for you to understand time goes up as computation goes up
-*/
+class animal {
+    constructor(name, legcount, speaks) {
+        this.name = name
+        this.legcount = legcount
+        this.speaks = speaks
 
-/// for 3////
-
-function calculateTime( n) {
-    let a = 0;
-    for (let i = 0; i<1000000000; i++){
-        a = a + i
     }
-    result = a; 
+speak(){
+     console.log("hi there" + this.speaks)
 }
 
-const beforedate = new Date();
-const beforetimeinMS = beforedate.getTime()
+}
+let dog = new animal("dog",4, "bhow bhow");
+let cat = new animal("cat",4,"meow meow");
 
-calculateTime(1000000000) 
-
-const afterdate = new Date();
-const aftertimeinMS = afterdate.getTime();
-console.log(aftertimeinMS - beforetimeinMS)
-
-//// answer 1373 ///
+cat.speak();
