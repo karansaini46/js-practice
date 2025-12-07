36eaad0d63d7191dc1e6fdf3b48c7dd70a0eadee
karansaini47 <karan0saini23@gmail.com>
2025-12-07 11:44:11 +0530

    fn callback

--- ass1.js
+++ ass1.js
@@ -1,5 +1,18 @@
-function currenttimeprint(){
-    console.log(new Date().getTime());
+function square (n){
+    return n * n ;
 }
+function cube (n){
+    return n*n*n 
+}
+function quad (n){
+    return n*n*n*n
+}
+ 
+function sumofsquares (a,b,fn) {
+let square1 = fn(a);
+let square2 = fn(b);
+return square1 + square2;
 
-setInterval(currenttimeprint, 1000);
+}
+let ans = sumofsquares(1,2,quad);
+console.log(ans)
